3f4032eebbe19c44debfcc42cc2aa11744d2d2db
Dan Ladendorf <dladendorf@gmail.com>
2018-08-22 18:23:21 -0600

    Added propTypes and made WeatherBlock component fully reusable

--- src/components/WeatherBlock.js
+++ src/components/WeatherBlock.js
@@ -32,42 +32,45 @@ export default class WeatherBlock extends React.Component {
         // https://cors-anywhere.herokuapp.com/ -- backup CORS proxy
         const CORS = 'http://cors-anywhere.dan.earth:8080/';
         const API_URL = 'https://api.darksky.net/forecast/';
-        const API_KEY = this.props.API_KEY || '0c7f10d0d5fa0d8602b3c9664767e7f7'; // Get from props
-        const cognizantLatLong = this.props.latLong || '40.016457,-105.285884';   // Get from props
-        const excludeBlocks = '?exclude=minutely,hourly,alerts,flags'; // exclude these from JSON response
+        const API_KEY = this.props.API_KEY || '0c7f10d0d5fa0d8602b3c9664767e7f7'; // Get from props or default to 
+        const cognizantLatLong = this.props.latLong || '40.016457,-105.285884';   // Get from props or default to Cognizant latLong
+        const excludeBlocks = '?exclude=minutely,hourly,alerts,flags'; // exclude these sections from JSON response
         
         if (date) {
           date = `,${date}`;
         }
    
         const forecastDay = this.props.forecastDay;
-
-        //https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/2f59170b5a75d855ff4dbbcfa4c498e0/40.016457,-105.285884?exclude=minutely,hourly,alerts,flags
+        // Example Dark Sky API Query:
+        // https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/2f59170b5a75d855ff4dbbcfa4c498e0/40.016457,-105.285884?exclude=minutely,hourly,alerts,flags
         
         axios.get(`${CORS}${API_URL}${API_KEY}/${cognizantLatLong}${date}${excludeBlocks}`)
             .then(response => { 
                 console.log(response);
                 const dsData = response.data;
-              if (forecastDay) {
-                const summary = dsData.daily.data[forecastDay].summary;
-                const skyIcon = dsData.daily.data[forecastDay].icon;
-                const temperature = Math.round(dsData.daily.data[forecastDay].temperatureHigh);
-                const tempHigh = Math.round(dsData.daily.data[forecastDay].temperatureHigh);
-                const tempLow = Math.round(dsData.daily.data[forecastDay].temperatureLow);
-                const time = this.toTextTime(dsData.daily.data[forecastDay].time);
 
-                this.setState({ skyIcon, summary, temperature, tempHigh, tempLow, doneLoading: true, timeMachineDate: time });
-                this.setState({ backgroundColor: this.getColor(temperature) });
-              } else {
-                const summary = dsData.currently.summary;
-                const skyIcon = dsData.currently.icon;
-                const temperature = Math.round(dsData.currently.temperature);
-                const tempHigh = Math.round(dsData.daily.data[0].temperatureHigh);
-                const tempLow = Math.round(dsData.daily.data[0].temperatureLow);
-
-                this.setState({ skyIcon, summary, temperature, tempHigh, tempLow, doneLoading: true });
-                this.setState({ backgroundColor: this.getColor(temperature) });
-              }
+                if (forecastDay) { // 3 or 7-Day Forecast
+                    this.setState({ 
+                        skyIcon: dsData.daily.data[forecastDay].icon,
+                        summary: dsData.daily.data[forecastDay].summary, 
+                        temperature: Math.round(dsData.daily.data[forecastDay].temperatureHigh),
+                        tempHigh: Math.round(dsData.daily.data[forecastDay].temperatureHigh),
+                        tempLow: Math.round(dsData.daily.data[forecastDay].temperatureLow),
+                        doneLoading: true, 
+                        timeMachineDate: this.toTextTime(dsData.daily.data[forecastDay].time),
+                        backgroundColor: this.getColor(Math.round(dsData.daily.data[forecastDay].temperatureHigh))
+                    });
+                } else { // Time Machine
+                    this.setState({ 
+                        skyIcon: dsData.currently.icon,
+                        summary: dsData.currently.summary,
+                        temperature: Math.round(dsData.currently.temperature),
+                        tempHigh: Math.round(dsData.daily.data[0].temperatureHigh),
+                        tempLow: Math.round(dsData.daily.data[0].temperatureLow),
+                        doneLoading: true,
+                        backgroundColor: this.getColor(Math.round(dsData.currently.temperature))
+                    });
+                }
             })
             .catch(err => {
                 console.log(err.message);
